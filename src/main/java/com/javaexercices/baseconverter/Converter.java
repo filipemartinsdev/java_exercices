1865c9bd25e4cf486437ac1565722aea7f8cb5a7
filipemartinsdev <dev.filipemartins@gmail.com>
2025-11-02 18:49:09 -0400

    feat: init new exercice [Converter]

---
+++ src/main/java/com/javaexercices/baseconverter/Converter.java
@@ -0,0 +1,48 @@
+package com.javaexercices.baseconverter;
+
+// EM DESENVOLVIMENTO
+
+public class Converter {
+
+    public static int intToBin(int number){
+        return number;
+    }
+    public static int intToOctal(int number){
+        return number;
+    }
+    public static String intToHex(int number){
+        return "";
+    }
+
+    public static int binToInt(int number){
+        String numberStr = number+"";
+
+        return number;
+    }
+    public static int binToOctal(int number){
+        return number;
+    }
+    public static String binToHex(int number){
+        return "";
+    }
+
+    public static int octalToInt(int number){
+        return number;
+    }
+    public static int octalToBin(int number){
+        return number;
+    }
+    public static String octalToHex(int number){
+        return "";
+    }
+
+    public static int hexToInt(String number){
+        return 0;
+    }
+    public static int hexToBin(int number){
+        return number;
+    }
+    public static int hexToOctal(int number){
+        return number;
+    }
+}
